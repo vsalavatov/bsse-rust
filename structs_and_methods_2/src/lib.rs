7c3a32efd9335f3d2ada432d7e679f6aabd9e6ee
Vadim Salavatov <vmsalavatov@gmail.com>
2021-09-16 22:32:23 +0300

    noisy_float + traits

--- structs_and_methods_2/src/lib.rs
+++ structs_and_methods_2/src/lib.rs
@@ -1,25 +1,104 @@
 pub mod geometry {
+    use std::fmt::{Debug, Write};
+
+    use noisy_float::prelude::*;
+
+    #[derive(Hash, PartialEq, Eq)]
     pub struct Point {
-        pub x: f64,
-        pub y: f64,
+        pub x: N64,
+        pub y: N64,
     }
 
+    impl Default for Point {
+        fn default() -> Self {
+            Point {
+                x: n64(0.0),
+                y: n64(0.0),
+            }
+        }
+    }
+
+    impl Debug for Point {
+        fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+            fmt.debug_struct("Point")
+                .field("x", &self.x)
+                .field("y", &self.y)
+                .finish()
+        }
+    }
+
+    #[derive(Hash, PartialEq, Eq)]
     pub struct Rect {
-        pub xleft: f64,
-        pub xright: f64,
-        pub ylow: f64,
-        pub yhigh: f64,
+        pub xleft: N64,
+        pub xright: N64,
+        pub ylow: N64,
+        pub yhigh: N64,
     }
 
+    impl Default for Rect {
+        fn default() -> Self {
+            Rect {
+                xleft: n64(0.0),
+                xright: n64(1.0),
+                ylow: n64(0.0),
+                yhigh: n64(1.0),
+            }
+        }
+    }
+
+    impl Debug for Rect {
+        fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+            fmt.debug_struct("Rectangle")
+                .field("x_left", &self.xleft)
+                .field("y_low", &self.ylow)
+                .field("x_right", &self.xright)
+                .field("y_high", &self.yhigh)
+                .finish()
+        }
+    }
+
+    #[derive(Hash, PartialEq, Eq)]
     pub struct Circle {
-        pub x: f64,
-        pub y: f64,
-        pub r: f64,
+        pub x: N64,
+        pub y: N64,
+        pub r: N64,
     }
 
+    impl Default for Circle {
+        fn default() -> Self {
+            Circle {
+                x: n64(0.0),
+                y: n64(0.0),
+                r: n64(1.0),
+            }
+        }
+    }
+
+    impl Debug for Circle {
+        fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+            fmt.debug_struct("Circle")
+                .field("x", &self.x)
+                .field("y", &self.y)
+                .field("radius", &self.r)
+                .finish()
+        }
+    }
+
+    #[derive(Hash, PartialEq, Eq)]
     pub enum Figure {
         Circle(Circle),
         Rect(Rect),
+    }
+
+    // I guess enumeration shouldn't have any default values in this case...
+
+    impl Debug for Figure {
+        fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+            match &self {
+                Figure::Circle(c) => fmt.write_str("Figure::").and_then(|_| c.fmt(fmt)),
+                Figure::Rect(r) => fmt.write_str("Figure::").and_then(|_| r.fmt(fmt)),
+            }
+        }
     }
 
     impl Rect {
@@ -30,21 +109,21 @@ pub mod geometry {
                 && p.y <= self.yhigh;
         }
 
-        pub fn area(&self) -> f64 {
+        pub fn area(&self) -> N64 {
             return (self.xright - self.xleft) * (self.yhigh - self.ylow);
         }
     }
 
     impl Circle {
         pub fn contains(&self, p: &Point) -> bool {
-            let dx: f64 = p.x - self.x;
-            let dy: f64 = p.y - self.y;
+            let dx: N64 = p.x - self.x;
+            let dy: N64 = p.y - self.y;
             let dist = (dx * dx + dy * dy).sqrt();
             return dist <= self.r;
         }
 
-        pub fn area(&self) -> f64 {
-            return std::f64::consts::PI * self.r * self.r;
+        pub fn area(&self) -> N64 {
+            return n64(std::f64::consts::PI) * self.r * self.r;
         }
     }
 
