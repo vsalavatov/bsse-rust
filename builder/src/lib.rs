d2a8fa924318e66174167890dddb688a73718277
Vadim Salavatov <vmsalavatov@gmail.com>
2021-11-28 18:54:34 +0300

    03

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -11,23 +11,32 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let builder_name = Ident::new(&format!("{}Builder", struct_name), Span::call_site());
 
     let mut builder_fields: Vec<TokenStream2> = vec![];
+    let mut builder_setters: Vec<TokenStream2> = vec![];
 
     if let syn::Data::Struct(data) = input.data {
         for field in data.fields {
-            let builder_field = Ident::new(
-                &format!("{}_value", field.ident.unwrap()),
-                Span::call_site(),
-            );
+            let ident = field.ident.unwrap();
+            let builder_field = Ident::new(&format!("{}_value", &ident), Span::call_site());
             let ty = field.ty;
             builder_fields.push(quote! {
                 #builder_field: std::option::Option<#ty>,
-            })
+            });
+            builder_setters.push(quote! {
+                pub fn #ident(&mut self, value: #ty) -> &mut Self {
+                    self.#builder_field = Some(value);
+                    self
+                }
+            });
         }
 
         let expanded = quote! {
             #[derive(Default)]
             pub struct #builder_name {
                 #(#builder_fields)*
+            }
+
+            impl #builder_name {
+                #(#builder_setters)*
             }
 
             impl #struct_name {
