1826548da202feb9a9528d6e341cd03e67068fce
Vadim Salavatov <vmsalavatov@gmail.com>
2021-11-28 18:21:34 +0300

    01

---
+++ builder/src/lib.rs
@@ -0,0 +1,9 @@
+use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
+
+#[proc_macro_derive(Builder)]
+pub fn derive(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    TokenStream::new()
+}
